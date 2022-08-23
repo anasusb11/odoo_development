78df56541e78ccd073677a1a8a5577b8e30931b8
anasusb11 <anasusb11@gmail.com>
2022-08-23 16:35:19 +0700

    add field in inherit views

--- om_hospital/models/models.py
+++ om_hospital/models/models.py
@@ -1,18 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class om_hospital(models.Model):
-#     _name = 'om_hospital.om_hospital'
-#     _description = 'om_hospital.om_hospital'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+    _description = 'Sale Order'
+    
+    sale_description = fields.Text('Sale Description')
