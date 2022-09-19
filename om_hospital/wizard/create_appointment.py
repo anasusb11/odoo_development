779817ebb7baa587eda1b011a553df8fdb501c05
anasusb11 <anasusb11@gmail.com>
2022-09-19 13:53:29 +0700

    update button wizard

--- om_hospital/wizard/create_appointment.py
+++ om_hospital/wizard/create_appointment.py
@@ -4,10 +4,13 @@ class CreateAppointmentWizard(models.TransientModel):
     _name = 'create.appointment.wizard'
     _description = 'Create Appointment Wizard'
 
-    name = fields.Char('Name', required=True, default="New")
+    date_appointment = fields.Date('Date Appointment')
     patient_id = fields.Many2one('hospital.patient', string='Patient')
 
     def action_create_appointment(self):
-        self.ensure_one()
-        pass
+        vals = {
+            'patient_id': self.patient_id.id,
+            'date_appointment': self.date_appointment
+            }
+        self.env['hospital.appointment'].create(vals)
     
