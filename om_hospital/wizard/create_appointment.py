1ffbbe3b932646e9a01e54e54a15a0bafdd82fbd
anasusb11 <anasusb11@gmail.com>
2022-09-19 16:10:32 +0700

    add return view function

--- om_hospital/wizard/create_appointment.py
+++ om_hospital/wizard/create_appointment.py
@@ -9,8 +9,39 @@ class CreateAppointmentWizard(models.TransientModel):
 
     def action_create_appointment(self):
         vals = {
+
+            # create patient from wizard
             'patient_id': self.patient_id.id,
             'date_appointment': self.date_appointment
             }
-        self.env['hospital.appointment'].create(vals)
-    
+        appointment_rec = self.env['hospital.appointment'].create(vals)
+        print("================================================", appointment_rec)
+        return {
+                'name': ('Appointment'),
+                'type': 'ir.actions.act_window',
+                'view_mode': 'form',
+                'res_model': 'hospital.appointment',
+                'res_id': appointment_rec.id
+            }
+    def action_view_appointment(self):
+        # Method 1
+        # action = self.env.ref('om_hospital.appointment_action').read()[0]
+        # action['domain'] = [('patient_id', '=', self.patient_id.id)]
+        # return action
+
+        # Method 2
+        # action = self.env['ir.actions.actions']._for_xml_id('om_hospital.appointment_action')
+        # action['domain'] = [('patient_id', '=', self.patient_id.id)]
+        # return action
+
+        # Method 3
+        return {
+                'name': ('Appointment'),
+                'type': 'ir.actions.act_window',
+                'view_type': 'form',
+                'view_mode': 'tree,form',
+                'res_model': 'hospital.appointment',
+                'target': 'current',
+                'domain': [('patient_id', '=', self.patient_id.id)]
+            }
+        # print("===============================",action)
